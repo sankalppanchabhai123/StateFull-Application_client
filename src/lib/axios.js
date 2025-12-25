e604ae3574559b9a168696a58b2201d6923edc62
Sankalp Panchabhai <147154199+sankalppanchabhai123@users.noreply.github.com>
2025-12-25 16:20:16 +0530

    Update axios.js

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api",    
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "https://state-full-application-server-yagf.vercel.app/api",    
     withCredentials: true,
 
 })
